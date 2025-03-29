edd800cfd3a7e46770f397622e2fbdb3d7d6e264
Mr.Pasindu <140577992+CyberSamuZaX@users.noreply.github.com>
2025-03-29 22:39:18 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'PKT',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "PAGNGNASIRI THERO",
+  botOwner: 'PAGNGNASIRI THERO', // Your name
+  ownerNumber: '94752677804', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
