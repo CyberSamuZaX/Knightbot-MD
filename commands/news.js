5cfabb0df3edb957ed2d4d3798cc09764534a578
Mr.Pasindu <140577992+CyberSamuZaX@users.noreply.github.com>
2025-03-30 07:37:25 +0530

    Update news.js

--- commands/news.js
+++ commands/news.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 module.exports = async function (sock, chatId) {
     try {
         const apiKey = 'apikey=Manul-Official-Key-3467';  // Replace with your NewsAPI key
-        const response = await axios.get(`https://mr-manul-ofc-apis.vercel.app/api/hiru-news?=us&apiKey=${apiKey`);
+        const response = await axios.get(`https://mr-manul-ofc-apis.vercel.app/api/hiru-news?${apiKey`);
         const articles = response.data.articles.slice(0, 5); // Get top 5 articles
         let newsMessage = '📰 *Latest News*:\n\n';
         articles.forEach((article, index) => {
